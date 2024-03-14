e569f85d25f691a005ade6653bdc949584411745
TG <titoironman@msn.com>
2024-03-15 02:00:43 +0300

    task 4 solution added

--- 4.js
+++ 4.js
@@ -18,4 +18,26 @@ console.log(sum(2, 6)); должно вывести число 8 в консол
 Демонстрировать работы функций не обязательно.
 */
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+console.log("Задание 4:");
+const a = inputVariable("Введите цифру 1 для сложения");
+const b = inputVariable("Введите цифру 2 для сложения");
+const sum = (a, b) => a + b;
+
+const c = inputVariable("Введите цифру 1 для вычисления разности");
+const d = inputVariable("Введите цифру 2 для вычисления разности");
+const difference = (c, d) => Math.max(c, d) - Math.min(c, d);
+
+const e = inputVariable("Введите цифру 1 для умножения");
+const f = inputVariable("Введите цифру 2 для умножения");
+const multiply = (e, f) => e * f;
+
+const g = inputVariable("Введите цифру 1 для деления");
+const h = inputVariable("Введите цифру 2 для деления");
+const divide = (g, h) => (h === 0 ? "Делилм на ноль." : g / h);
+
+console.log(`Сложение: ${sum(a, b)}`);
+console.log(`Разность: ${difference(c, d)}`);
+console.log(`Умножение: ${multiply(e, f)}`);
+console.log(`Деление: ${divide(g, h)}`);
+
+console.log("-------------------------------------");
